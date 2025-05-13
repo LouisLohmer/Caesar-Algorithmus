704253d730fab98353ce705690c9b1a3e691ff8d
LonelyStreetracer <126259607+LouisLohmer@users.noreply.github.com>
2025-05-13 18:59:13 +0200

    add copy-to-clipboard button

--- caesar-algorithm/src/components/encryptTextComponent.vue
+++ caesar-algorithm/src/components/encryptTextComponent.vue
@@ -35,6 +35,7 @@ function decryptText() {
   let decryptionKey = optionElements[optionElements.selectedIndex].id;
   let decryptedTextElement = document.getElementById("decrypted-text");
   let decryptedText = "";
+  let copyToClipboardButton = document.getElementById("copy-to-clipboard");
 
   for (let i = 0; i < encryptedText.length; i++) {
     let encryptedLetter = encryptedText[i].toLowerCase();
@@ -56,6 +57,7 @@ function decryptText() {
   }
 
   decryptedTextElement.innerHTML = decryptedText;
+  copyToClipboardButton.style.visibility = "visible";
 }
 
 function validateUserInput() {
@@ -80,6 +82,18 @@ function validateUserInput() {
   }
 
   decryptText();
+}
+
+function copyDecryptedTextToClipboard() {
+  let decryptedText = document.getElementById("decrypted-text").innerHTML;
+  let successsMessage = document.getElementById("success-message");
+
+  navigator.clipboard.writeText(decryptedText);
+  successsMessage.classList.remove("hidden");
+
+  setTimeout(() => {
+    successsMessage.classList.add("hidden");
+  }, 5000);
 }
 </script>
 
@@ -124,9 +138,32 @@ function validateUserInput() {
     </div>
     <div class="decrypyeted-text-container">
       <h2>Entschlüsselter Text</h2>
-      <span class="decrypted-text" id="decrypted-text"
-        >Es wurde noch kein Text entschlüsselt</span
+      <span class="success-message hidden" id="success-message"
+        >Text kopiert!</span
       >
+      <div class="result-container">
+        <button
+          class="copy-to-clipboard"
+          id="copy-to-clipboard"
+          @click="copyDecryptedTextToClipboard"
+        >
+          <svg
+            class="copy-to-clipboard-icon"
+            clip-rule="evenodd"
+            fill-rule="evenodd"
+            stroke-linejoin="round"
+            stroke-miterlimit="2"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="m6 19v2c0 .621.52 1 1 1h2v-1.5h-1.5v-1.5zm7.5 3h-3.5v-1.5h3.5zm4.5 0h-3.5v-1.5h3.5zm4-3h-1.5v1.5h-1.5v1.5h2c.478 0 1-.379 1-1zm-1.5-1v-3.363h1.5v3.363zm0-4.363v-3.637h1.5v3.637zm-13-3.637v3.637h-1.5v-3.637zm11.5-4v1.5h1.5v1.5h1.5v-2c0-.478-.379-1-1-1zm-10 0h-2c-.62 0-1 .519-1 1v2h1.5v-1.5h1.5zm4.5 1.5h-3.5v-1.5h3.5zm3-1.5v-2.5h-13v13h2.5v-1.863h1.5v3.363h-4.5c-.48 0-1-.379-1-1v-14c0-.481.38-1 1-1h14c.621 0 1 .522 1 1v4.5h-3.5v-1.5z"
+              fill-rule="nonzero"
+            />
+          </svg>
+        </button>
+        <span class="decrypted-text" id="decrypted-text"></span>
+      </div>
     </div>
   </div>
 </template>
